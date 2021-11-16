6c245976e909f956d471aa8009b0a8933f724c70
tanish223 <94443941+tanish223@users.noreply.github.com>
2021-11-16 15:04:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+ var box ;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(70);
+  if (keyIsDown(RIGHT_ARROW)) {
+    box.position.x =box.position.x +5;
+  }
+
+  drawSprites();
+}
+
+
+
+
